7b8e5f19051c69c43401e75856a976b1472e5c97
gabimoisa1998 <gabriel.moisa@opswat.com>
2021-11-22 15:20:46 +0200

    node

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ var app = express();
 //app.get("/", (request, response) => response.sendFile(__dirname + "/public/style.css"));
 
 
-app.get(
+/*app.get(
     "/now",
     (req, res, next) => {
       req.time = new Date().toString();
@@ -16,9 +16,18 @@ app.get(
         time: req.time
       });
     }
-  );
+  );*/
 
-
+  app.get("/name", function(req, res) {
+    var firstName = req.query.first;
+    var lastName = req.query.last;
+    // OR you can destructure and rename the keys
+    var { first: firstName, last: lastName } = req.query;
+    // Use template literals to form a formatted string
+    res.json({
+      name: `${firstName} ${lastName}`
+    });
+  });
 
 
 
