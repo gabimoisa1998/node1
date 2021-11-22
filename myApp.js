fa3de7e1466f1fca145671327c1ceb8c2de3ce5f
gabimoisa1998 <gabriel.moisa@opswat.com>
2021-11-22 14:54:27 +0200

    node

--- myApp.js
+++ myApp.js
@@ -2,7 +2,20 @@ var express = require('express');
 var app = express();
 //console.log("HellO World");
 
-app.get("/", (request, response) => response.sendFile(__dirname + "/public/style.css"));
+//app.get("/", (request, response) => response.sendFile(__dirname + "/public/style.css"));
+
+app.get(
+    "/now",
+    (req, res, next) => {
+      req.time = new Date().toString();
+      next();
+    },
+    (req, res) => {
+      res.send({
+        time: req.time
+      });
+    }
+  );
 
 
 
