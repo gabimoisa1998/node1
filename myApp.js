e5249ecc5a70322253b765795391b7dcd9fb9382
gabimoisa1998 <gabriel.moisa@opswat.com>
2021-11-22 16:13:28 +0200

    node

--- myApp.js
+++ myApp.js
@@ -18,12 +18,21 @@ var app = express();
     }
   );*/
 
-  app.get('/:word/hello', (req, res)=>{
+  /*app.get('/:word/hello', (req, res)=>{
       let word = req.params.word
       res.json({echo : word})
-  })
+  })*/
 
-  
+  app.get("/name", function(req, res) {
+    var firstName = req.query.first;
+    var lastName = req.query.last;
+    // OR you can destructure and rename the keys
+    var { first: firstName, last: lastName } = req.query;
+    // Use template literals to form a formatted string
+    res.json({
+      name: `${firstName} ${lastName}`
+    });
+  });
 
   
 
