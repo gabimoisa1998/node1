34011e810a0c7c173d68a63fd398e4f7c3e1d2b8
gabimoisa1998 <gabriel.moisa@opswat.com>
2021-11-22 14:12:45 +0200

    node

--- myApp.js
+++ myApp.js
@@ -2,8 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use(function middleware(req, res, next) {
-    // Do something
-    // Call the next function in line:
+    console.log("I'm a middleware...");
     next();
   });
 
