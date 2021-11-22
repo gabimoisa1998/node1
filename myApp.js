d9fa80a03ba385cbc5d86eeff022cbea529e348b
gabimoisa1998 <gabriel.moisa@opswat.com>
2021-11-22 11:50:50 +0200

    node

--- myApp.js
+++ myApp.js
@@ -2,9 +2,7 @@ var express = require('express');
 var app = express();
 console.log("Hello World");
 
-app.get("/", function(req, res) {
-    res.send("Hello Express");
-  });
+
 
   app.get("/", function(req, res) {
     res.sendFile(__dirname + "/public/index.html");
